96c967bf2e0d65882f3f68c75b513d4ab445cdb9
Ciaran Gallagher <ciaran@thegallagherclan.co.uk>
2020-03-26 12:26:49 +0000

    Changes styles and text

--- js/main.js
+++ js/main.js
@@ -53,44 +53,6 @@ $(document).ready(() => {
             $("#btnConnectToPlex").prop("disabled", true);
         }
     });
-
-    // Show the Welcome Modal
-    $('#progressModal #progressModalTitle').empty();
-    $('#progressModal #progressModalTitle').text(`Welcome to PASTA`);
-    $('#progressModal #modalBodyText').empty();
-    $('#progressModal #modalBodyText').append(`
-        <h3>What is PASTA?</h3>
-        <p>Do you watch TV Shows with multiple languages and subtitles and wish you could change them for the entire show, rather than needing to do it for <em>every. single. episode</em>? 
-        Or maybe you aren't sure what the difference is between those 2 <strong>Enlish (SRT)</strong> and <strong>English (SRT)</strong> subtitle files. Then PASTA is for you!
-        <br>PASTA allows you to connect to your Plex server and view more details about the audio tracks and subtitles, as well as set the tracks and subtitles
-        for entire shows, or single episodes very quickly.</p>
-        <h3>How do I use PASTA?</h3>
-        <p>I built PASTA to be as step-by-step as possible and to take you through it, so you should be able to just close this pop-up and follow along.
-        <br>There are some things I would like to point out, however:</p>
-        <ul>
-            <li>This currently does <strong>not</strong> support logging in via username / password (I will look into that at a later date). Currently you must use
-            the IP address, or if your Plex server is addressable by a valid name address, then use that.</li>
-            <li>You must use the Plex Token for authentication. It's the only way I have this working so far. I've included a link with instructions
-            on how to get a Plex Token below the required field.</li>
-            <li>This works <strong>MUCH</strong> faster if you are on the same network as the Plex Server.</li>
-            <li>You can also run this locally yourself. Just download the source code from github (see the next section).</li>
-        </ul>
-        <h3>About PASTA</h3>
-        <p>PASTA is short for <em>Plex Audio and Subtitle Track Automation</em>. When I first began developing this for myself, I was calling it
-        <em>Plex Audio Track Automation</em>, so adding subtitles to it gave birth to PASTA.<p>
-        <p>PASTA was born out of a desire, one that I had seen others have as well, but that I had only seen one other solution for. However, it was in
-        command line and I wanted something a bit more appealing to look at, and something I could use from anywhere.
-        Initially I was only building this for myself but I thought that others might find use for it as well, so here we are!</p>
-        <p>PASTA runs entirely client-side. This means that you are not passing anything to someones server to do this (other than the Plex Server), and it also means I don't
-        have to worry about standing up a server to do that side of things either :). PASTA runs off of Github Pages, and I've got a link to my
-        repository below. Feel free to have a look, download it yourself and use it locally, or make suggestions. I'm by no means finished with
-        PASTA - I still have plenty of ideas for how I can add more to it, as well as fix any bugs that crop up. Feel free to log any issues / feature 
-        requests on the Github as well.</p>
-        <p><a href="https://github.com/cglatot/pasta" target="_blank">https://github.com/cglatot/pasta</a></p>
-        <h3>Cookie Policy</h3>
-        <p>The site uses some minor cookies to help improve your experience. Third-party cookies are not used on this site. By continuing to use this site, you agree to the use of cookies.</p>
-        `);
-    $('#progressModal').modal();
 });
 
 function connectToPlex() {
